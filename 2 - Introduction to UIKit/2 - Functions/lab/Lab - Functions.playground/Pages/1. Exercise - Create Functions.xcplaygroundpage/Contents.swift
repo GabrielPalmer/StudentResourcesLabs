79f41ff6c947bedbfcf259605ed1d720ac86e65e
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2018-09-05 10:03:04 -0600

    Lab 2 complete

--- 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,33 @@
  
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the funtion and observe the printout.
  */
-
+func introduceMyself() {
+    print("My name is Gabe and I like to read!")
+}
 
 /*:
  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = arc4random_uniform(UInt32(5))` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts. Note that the random number generation will not work unless your function is declared after the `import Foundation` statement below. This is because the `arc4random_uniform(_:)` function is declared in the `Foundation` framework.
  */
 import Foundation
 
+func magicEightBall() {
+    
+    let randomNum = arc4random_uniform(UInt32(5))
+    
+    switch randomNum {
+    case 0:
+        print("Change one of your recent decisions")
+    case 1:
+        print("Why are you asking me?")
+    case 2:
+        print("Ask again when your question actually matters")
+    case 3:
+        print("Yep, do that or you'll regret it")
+    case 4:
+        print("Nope, that path will probably lead to getting eaten by space worms")
+    default:
+        print("Your eight ball is not actually magic")
+    }
+}
 
 //: page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
--- 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -10,10 +10,38 @@
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
 
+func progressUpdate(steps: Int, goal: Int) {
+    
+    if steps < goal / 10 {
+        print("Your off to a good start.")
+    } else if steps < goal / 2 {
+        print("Your almost halfway there!")
+    } else if steps < goal - goal / 10 {
+        print("Your over halfway there!")
+    } else if steps < goal {
+        print("Your almost there!")
+    } else {
+        print("You beat your goal!")
+    }
+}
+
+progressUpdate(steps: 350, goal: 10000)
+progressUpdate(steps: 2400, goal: 10000)
+progressUpdate(steps: 7800, goal: 10000)
+progressUpdate(steps: 9998, goal: 10000)
+progressUpdate(steps: 12000, goal: 10000)
 
 /*:
  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
  */
-
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    
+    //if your percentage of total distance traveled so far is greater than percentage of total time passed then your current pace will reach goal before or on time.
+    if currentDistance / totalDistance > currentTime / goalTime {
+        print("Keep it up!")
+    } else {
+        print("You'be got to push it just a bit harder!")
+    }
+}
 
 //: [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
